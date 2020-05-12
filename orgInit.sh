0034b56a8f2afbbc9f7353c54516914b72492863
samcfman <sman@sman-ltm1.internal.salesforce.com>
2020-05-12 20:05:38 +0800

    commit

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix blitz --userdomain back.log
+sfdx shane:org:create -f config/project-scratch-def.json -s -d 1 --userprefix dev --userdomain citi.demo
 sfdx force:source:push
 sfdx force:user:permset:assign --permsetname citipermissionset
 sfdx force:data:tree:import --plan sfdx-out/export-demo-Account-plan.json
